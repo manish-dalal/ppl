96aa60acc2ee83587e29b1a4fed7a7790cd305cd
Manish Dalal <md4640@gmail.com>
2018-07-15 15:17:26 +0530

    blaaaaaaaaaaaaa........

--- src/Login_Register.js
+++ src/Login_Register.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Login from './Login'
 import Register from './Register';
-import {Switch,Route,Redirect} from 'react-router-dom';
+import {Switch,Route} from 'react-router-dom';
 
 class Login_Register extends Component {
    
@@ -11,7 +11,6 @@ class Login_Register extends Component {
         <Switch>
             <Route path='/user/register' component={Register}/>
             <Route path='/user/login' component={Login}/>
-            <Route path="/" component={() => <Redirect to="/user/register" />} />
         </Switch>
           
         </div>
